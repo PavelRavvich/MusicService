540a846ce772fc71e9e692cd2eaa7c5f04ac2d8f
Pavel <triodjangopiter@yandex.ru>
2017-07-06 06:40:52 +0300

    add Client - address model

---
+++ src/main/java/ru/pravvich/model/Address.java
@@ -0,0 +1,25 @@
+package ru.pravvich.model;
+
+/**
+ * Created by pavel on 04.07.17.
+ */
+public class Address {
+    private String country;
+    private String city;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
--- src/main/java/ru/pravvich/model/Client.java
+++ src/main/java/ru/pravvich/model/Client.java
@@ -1,7 +1,50 @@
 package ru.pravvich.model;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 /**
  * Created by pavel on 03.07.17.
  */
 public class Client {
+
+    private Address address;
+
+    private List<String> musics;
+
+    private Timestamp create;
+
+    private String role;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<String> getMusics() {
+        return musics;
+    }
+
+    public void setMusics(List<String> musics) {
+        this.musics = musics;
+    }
+
+    public Timestamp getCreate() {
+        return create;
+    }
+
+    public void setCreate(Timestamp create) {
+        this.create = create;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
