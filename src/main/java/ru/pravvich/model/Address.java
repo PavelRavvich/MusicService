82f23f3ab8f411e24423afcb6e74335c82e1d837
Pavel <triodjangopiter@yandex.ru>
2017-07-06 18:18:47 +0300

    add Address model

--- src/main/java/ru/pravvich/model/Address.java
+++ src/main/java/ru/pravvich/model/Address.java
@@ -7,6 +7,14 @@ public class Address {
     private String country;
     private String city;
 
+    public Address() {
+    }
+
+    public Address(String country, String city) {
+        this.country = country;
+        this.city = city;
+    }
+
     public String getCountry() {
         return country;
     }
