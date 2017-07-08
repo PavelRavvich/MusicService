95ccfbddafcb3ada08b544d73bdaf8cde7d25d7d
Pavel <triodjangopiter@yandex.ru>
2017-07-08 22:08:08 +0300

    add servlet for eddition

--- src/main/java/ru/pravvich/servlet/EditMusicServlet.java
+++ src/main/java/ru/pravvich/servlet/EditMusicServlet.java
@@ -1,7 +1,20 @@
 package ru.pravvich.servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by pavel on 08.07.17.
  */
-public class EditMusicServlet {
+public class EditMusicServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        req.getRequestDispatcher("/WEB-INF/views/EditMusic.jsp")
+                .forward(req, resp);
+    }
 }
