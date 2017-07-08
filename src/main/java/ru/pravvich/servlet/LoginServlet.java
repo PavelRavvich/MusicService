da704aac19e436633e1d2676ca18eb2a7d0f7403
Pavel <triodjangopiter@yandex.ru>
2017-07-08 22:08:53 +0300

    add LoginServlet

--- src/main/java/ru/pravvich/servlet/LoginServlet.java
+++ src/main/java/ru/pravvich/servlet/LoginServlet.java
@@ -1,7 +1,24 @@
 package ru.pravvich.servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by pavel on 08.07.17.
  */
-public class LoginServlet {
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        req.getRequestDispatcher("/Login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
