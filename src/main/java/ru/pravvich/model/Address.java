17148f8c710a5bcf02e7f2fe49079566e0153c8e
Pavel <triodjangopiter@yandex.ru>
2017-07-08 22:04:35 +0300

    modyfied address model

--- src/main/java/ru/pravvich/model/Address.java
+++ src/main/java/ru/pravvich/model/Address.java
@@ -6,13 +6,32 @@ package ru.pravvich.model;
 public class Address {
     private String country;
     private String city;
+    private int id;
 
     public Address() {
+    }
+
+    public Address(int id) {
+        this.id = id;
     }
 
     public Address(String country, String city) {
         this.country = country;
         this.city = city;
+    }
+
+    public Address(String country, String city, int id) {
+        this.country = country;
+        this.city = city;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getCountry() {
@@ -30,4 +49,5 @@ public class Address {
     public void setCity(String city) {
         this.city = city;
     }
+
 }
