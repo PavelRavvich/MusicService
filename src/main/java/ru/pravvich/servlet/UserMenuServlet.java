44afec3508e4dda1856f48b7f78f84d16ac92391
Pavel <triodjangopiter@yandex.ru>
2017-07-08 22:09:53 +0300

    add User Menu Servlet

--- src/main/java/ru/pravvich/servlet/UserMenuServlet.java
+++ src/main/java/ru/pravvich/servlet/UserMenuServlet.java
@@ -1,7 +1,35 @@
 package ru.pravvich.servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by pavel on 08.07.17.
  */
-public class UserMenuServlet {
+public class UserMenuServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        final String act = req.getParameter("act");
+
+        if (act.equals("edit")) {
+
+            req.getRequestDispatcher("/user_menu/edit_music")
+                    .forward(req, resp);
+
+        } else if (act.equals("add")) {
+
+            req.getRequestDispatcher("/WEB-INF/views/AddMusic.jsp")
+                    .forward(req, resp);
+
+        } else if (act.equals("delete")){
+
+            req.getRequestDispatcher("/WEB-INF/views/DeleteMusic.jsp")
+                    .forward(req, resp);
+        }
+    }
 }
