ad4a49204d8291d52d2a42a8a00c6f729f8ebbc6
Pavel <triodjangopiter@yandex.ru>
2017-07-09 06:10:06 +0300

    add ajax dynamic refer country-city

--- src/main/webapp/js/location_refer.js
+++ src/main/webapp/js/location_refer.js
@@ -1,3 +1,19 @@
 /**
- * Created by pavel on 08.07.17.
+ * Update location sets without update all page.
+ * When select country then update set of cities.
  */
+$(document).ready(function() {
+    $('#country').change(function(event) {
+        var country = $("select#country").val();
+        $.get('citiesByCountry', {
+            country : country
+        }, function(response) {
+
+            var select = $('#city');
+            select.find('option').remove();
+            $.each(response, function(index, value) {
+                $('<option>').val(value).text(value).appendTo(select);
+            });
+        });
+    });
+});
